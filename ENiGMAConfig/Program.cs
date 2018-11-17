15dbdfa67abdebb9a89fcbbb91f54634ab80668c
Jason Windisch <Jason.Windisch@Gmail.com>
2018-11-17 14:39:32 -0500

    Save selected logging level

--- ENiGMAConfig/Program.cs
+++ ENiGMAConfig/Program.cs
@@ -59,7 +59,7 @@ namespace ENiGMAConfig
 
         private static void About()
         {
-            MessageBox.Query(60, 7, "About","ddd", "ok");
+            MessageBox.Query(60, 7, "About", "ddd", "ok");
         }
 
         private static void AddConfigMenu()
@@ -86,7 +86,6 @@ namespace ENiGMAConfig
             TextField TextFieldTelnetPort = new TextField(24, 0, 5, MainConfig.Telnet.Qstr("port"));
             TextFieldTelnetPort.Changed += new EventHandler(UpdateTelnetPort);
 
-
             CheckBox CheckBoxSSH = new CheckBox(1, 1, "ssh");
             CheckBoxSSH.Checked = MainConfig.SSH.Qb("enabled");
             CheckBoxSSH.Toggled += new EventHandler(UpdateSSHEnabled);
@@ -105,12 +104,14 @@ namespace ENiGMAConfig
 
             CheckBox CheckBoxWS = new CheckBox(1, 6, "ws");
             CheckBoxWS.Checked = MainConfig.WSS.Qb("enabled");
+            CheckBoxWS.Toggled += new EventHandler(UpdateWSEnabled);
 
             Label LabelWSPort = new Label(17, 6, "port: ");
             TextField TextFieldWSPort = new TextField(24, 6, 5, MainConfig.WS.Qstr("port"));
 
             CheckBox CheckBoxWSS = new CheckBox(1, 7, "wss");
             CheckBoxWSS.Checked = MainConfig.WSS.Qb("enabled");
+            CheckBoxWSS.Toggled += new EventHandler(UpdateWSSEnabled);
 
             Label LabelWSSPort = new Label(17, 7, "port: ");
             TextField TextFieldWSSPort = new TextField(24, 7, 5, MainConfig.WSS.Qstr("port"));
@@ -126,6 +127,7 @@ namespace ENiGMAConfig
             TextField TextFieldLogPath = new TextField(13, 2, 22, MainConfig.Paths.Qs("logs"));
             Label LabelLogLevel = new Label(39, 0, "Level: ");
             RadioGroup RadioGroupDebug = new RadioGroup(39, 2, new[] { "_Error", "_Warn", "_Info", "_Debug", "_Trace" });
+
             switch (MainConfig.Logging.Qs("level"))
             {
                 case "error":
@@ -153,31 +155,73 @@ namespace ENiGMAConfig
                         RadioGroupDebug.Selected = 4;
                         break;
                     }
-                default:
+                default: //If missing or invalid - Set to info.
                     {
                         RadioGroupDebug.Selected = 2;
                         break;
                     }
             }
-            
+
+            RadioGroupDebug.SelectionChanged += (int x) => UpdateLogLevel(x);
 
             FrameViewLogging.Add(LabelFilename, TextFieldFileame, LabelLogPath, TextFieldLogPath, LabelLogLevel, RadioGroupDebug);
 
             win.Add(FrameViewLoginServers, FrameViewLogging, Footer);
         }
 
+        private static void UpdateLogLevel(int Selected)
+        {
+            // "_Error", "_Warn", "_Info", "_Debug", "_Trace"
+            switch (Selected)
+            {
+                case 0:
+                        MainConfig.Logging["level"] = "error";
+                        break;
+                case 1:
+                        MainConfig.Logging["level"] = "warn";
+                        break;
+                case 2:
+                        MainConfig.Logging["level"] = "info";
+                        break;
+                case 3:
+                        MainConfig.Logging["level"] = "debug";
+                        break;
+                case 4:
+                        MainConfig.Logging["level"] = "trace";
+                        break;
+                default: //If missing or invalid - Set to info.
+                    
+                        MainConfig.Logging["level"] = "info";
+                        break;   
+            }
+        }
+
+        private static void UpdateLoggingLevel(int Level)
+        {
+        }
+
         private static void UpdateSSHEnabled(object sender, EventArgs e)
         {
             CheckBox SSH = (CheckBox)sender;
             MainConfig.SSH["enabled"] = SSH.Checked;
-
         }
 
         private static void UpdateTelnetEnabled(object sender, EventArgs e)
         {
             CheckBox Telnet = (CheckBox)sender;
             MainConfig.Telnet["enabled"] = Telnet.Checked;
+        }
 
+        private static void UpdateWSEnabled(object sender, EventArgs e)
+        {
+            CheckBox WS = (CheckBox)sender;
+            MainConfig.WS["enabled"] = WS.Checked;
+        }
+
+        private static void UpdateWSSEnabled(object sender, EventArgs e)
+        {
+            CheckBox WSS = (CheckBox)sender;
+            MainConfig.WSS["enabled"] = WSS.Checked;
         }
 
         private static void UpdateSSHPort(object sender, EventArgs e)
@@ -192,7 +236,6 @@ namespace ENiGMAConfig
             MainConfig.Telnet["port"] = TF.Text.ToString();
         }
 
-
         private static void UpdateBBSName(object sender, EventArgs e)
         {
             TextField TF = (TextField)sender;
@@ -201,7 +244,7 @@ namespace ENiGMAConfig
             string f = TF.Text.ToString();
 
             MainConfig.General["boardName"] = f;
-          //  TF.Text = "UpdateCheck";
+            //  TF.Text = "UpdateCheck";
         }
 
         private static void Save()
@@ -212,22 +255,32 @@ namespace ENiGMAConfig
         private static void SaveAs()
         {
             SaveDialog SaveD = new SaveDialog("Save As", "Save as file");
-            SaveD.DirectoryPath = @"C:\Users\jasin\source\repos\EnigmaConfig\";
+            SaveD.DirectoryPath = Directory.GetCurrentDirectory(); //Use current path for now. Switch to default config
 
             Application.Run(SaveD);
+            // SaveD.
             string Filename = SaveD.FileName.ToString();
             if (string.IsNullOrWhiteSpace(Filename))
             {
                 return;
             }
-            string FullSavePath = SaveD.DirectoryPath.ToString() + Filename;
+            string SaveDir = "";
+            if (SaveD.DirectoryPath.EndsWith("\\"))
+            {
+                SaveDir = SaveD.DirectoryPath.ToString();
+            }
+            else
+            {
+                SaveDir = SaveD.DirectoryPath.ToString() + "\\";
+            }
+            string FullSavePath = SaveDir + Filename;
             SaveJson(FullSavePath);
         }
 
         private static void Open()
         {
             OpenDialog OpenD = new OpenDialog("Open", "Open a file");
-            OpenD.DirectoryPath = @"C:\Users\jasin\source\repos\EnigmaConfig\";
+            OpenD.DirectoryPath = OpenD.DirectoryPath = Directory.GetCurrentDirectory();
             OpenD.AllowsMultipleSelection = false;
 
             Application.Run(OpenD);
@@ -259,7 +312,7 @@ namespace ENiGMAConfig
         {
             ClearViews();
 
-            win.Add(LabelNew,Footer);
+            win.Add(LabelNew, Footer);
         }
 
         private static void NewFile()
@@ -309,7 +362,6 @@ namespace ENiGMAConfig
 
         private static void SaveJson(string FilePath)
         {
-
             HjsonOptions FileOptions = new HjsonOptions();
             FileOptions.KeepWsc = true;
 
