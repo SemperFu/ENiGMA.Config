d676c82faf99a88b2e76acbe41d6b7c1701ece2c
Jason <jason.windisch@gmail.com>
2020-11-13 20:39:22 -0500

    Cleanup/Reorg

---
+++ ENiGMAConfig/MenuChanges.cs
@@ -0,0 +1,273 @@
+﻿using NStack;
+using System;
+using Terminal.Gui;
+
+namespace ENiGMAConfig
+{
+    partial class Program
+    {
+        private static void UpdateBBSName(ustring e)
+        {
+            //Name validation if needed
+            string f = e.ToString();
+
+            MainConfig.General["boardName"] = f;
+            //  TF.Text = "UpdateCheck";
+        }
+
+        private static void UpdateMenuFile(ustring e)
+        {
+            string f = e.ToString();
+            MainConfig.General["menuFile"] = f;
+        }
+
+        private static void UpdatePromptFile(ustring e)
+        {
+            MainConfig.General["promptFile"] = e.ToString();
+        }
+
+        private static void UpdateDefaultTheme(ustring e)
+        {
+            MainConfig.Theme["default"] = e.ToString();
+        }
+
+        private static void UpdatePreloginTheme(ustring e)
+        {
+            MainConfig.Theme["preLogin"] = e.ToString();
+        }
+
+        private static void UpdateTelnetEnabled(bool TelnetChecked)
+        {
+            MainConfig.LoginServersTelnet["enabled"] = TelnetChecked;
+        }
+
+        private static void UpdateSSHEnabled(bool SSHChecked)
+        {
+            MainConfig.LoginServersSSH["enabled"] = SSHChecked;
+        }
+
+        private static void UpdateWSEnabled(bool WSChecked)
+        {
+            MainConfig.LoginServersWS["enabled"] = WSChecked;
+        }
+
+        private static void UpdateWSSEnabled(bool WSSChecked)
+        {
+            MainConfig.LoginServersWSS["enabled"] = WSSChecked;
+        }
+
+        private static void UpdatePrivateKeyPath(ustring e)
+        {
+            MainConfig.LoginServersSSH["privateKeyPem"] = e.ToString();
+        }
+
+        private static void UpdatePrivateKeyPass(ustring e)
+        {
+            MainConfig.LoginServersSSH["privateKeyPass"] = e.ToString();
+        }
+
+        private static void UpdateSSHPort(TextField SSHPort, ustring OldText)
+        {
+            if (SharedFunctions.PortCheck(SSHPort.Text))
+            {
+                var cp = SSHPort.CursorPosition;
+                SSHPort.Text = OldText;
+                SSHPort.CursorPosition = Math.Min(cp, SSHPort.Text.RuneCount);
+            }
+
+            int DefaultPort = 8889; //default port
+
+            if (Int32.TryParse(SSHPort.Text.ToString(), out int ParsedPort))
+            {
+                if (ParsedPort > 65535) SSHPort.Text = "65535";
+                MainConfig.LoginServersSSH["port"] = ParsedPort;
+            }
+            else
+            {
+                MainConfig.LoginServersSSH["port"] = DefaultPort;
+            }
+        }
+
+        private static void UpdateTelnetPort(TextField TelnetPort, ustring OldText)
+        {
+            // Don't allow more than 4 digits and Match Digits only
+            if (SharedFunctions.PortCheck(TelnetPort.Text))
+            {
+                var cp = TelnetPort.CursorPosition;
+                TelnetPort.Text = OldText;
+                TelnetPort.CursorPosition = Math.Min(cp, TelnetPort.Text.RuneCount);
+            }
+
+            int DefaultPort = 8888; //default port
+
+            if (Int32.TryParse(TelnetPort.Text.ToString(), out int ParsedPort))
+            {
+                if (ParsedPort > 65535) TelnetPort.Text = "65535";
+                MainConfig.LoginServersTelnet["port"] = ParsedPort;
+            }
+            else
+            {
+                MainConfig.LoginServersTelnet["port"] = DefaultPort;
+            }
+        }
+
+        private static void UpdateWSPort(TextField WSPort, ustring OldText)
+        {
+            // Don't allow more than 4 digits and Match Digits only
+            if (SharedFunctions.PortCheck(WSPort.Text))
+            {
+                var cp = WSPort.CursorPosition;
+                WSPort.Text = OldText;
+                WSPort.CursorPosition = Math.Min(cp, WSPort.Text.RuneCount);
+            }
+
+            int DefaultPort = 8810; //default port
+
+            if (Int32.TryParse(WSPort.Text.ToString(), out int ParsedPort))
+            {
+                if (ParsedPort > 65535) WSPort.Text = "65535";
+                MainConfig.LoginServersWS["port"] = ParsedPort;
+            }
+            else
+            {
+                MainConfig.LoginServersWS["port"] = DefaultPort;
+            }
+        }
+
+        private static void UpdateWSSPort(TextField WSSPort, ustring OldText)
+        {
+            // Don't allow more than 4 digits and Match Digits only
+            if (SharedFunctions.PortCheck(WSSPort.Text))
+            {
+                var cp = WSSPort.CursorPosition;
+                WSSPort.Text = OldText;
+                WSSPort.CursorPosition = Math.Min(cp, WSSPort.Text.RuneCount);
+            }
+
+            int DefaultPort = 8811; //default port
+
+            if (Int32.TryParse(WSSPort.Text.ToString(), out int ParsedPort))
+            {
+                if (ParsedPort > 65535) WSSPort.Text = "65535";
+                MainConfig.LoginServersWSS["port"] = ParsedPort;
+            }
+            else
+            {
+                MainConfig.LoginServersWSS["port"] = DefaultPort;
+            }
+        }
+
+        private static void ButtonEKeyShow_Clicked(Button ButtonEKeyShow, TextField TextFieldEmailAuthPass)
+        {
+            if (TextFieldEmailAuthPass.Secret)
+            {
+                TextFieldEmailAuthPass.Secret = false;
+                ButtonEKeyShow.Text = "Hide";
+            }
+            else
+            {
+                TextFieldEmailAuthPass.Secret = true;
+                ButtonEKeyShow.Text = "Show";
+            }
+        }
+
+        private static void ButtonPKeyShow_Clicked(Button ButtonPKeyShow, TextField TextFieldPrivateKeyPass)
+        {
+            if (TextFieldPrivateKeyPass.Secret)
+            {
+                TextFieldPrivateKeyPass.Secret = false;
+                ButtonPKeyShow.Text = "Hide";
+            }
+            else
+            {
+                TextFieldPrivateKeyPass.Secret = true;
+                ButtonPKeyShow.Text = "Show";
+            }
+        }
+
+        private static void UpdateLogFileame(ustring e)
+        {
+            MainConfig.Logging["fileName"] = e.ToString();
+        }
+
+        private static void UpdateLogPath(ustring e)
+        {
+            MainConfig.Paths["logs"] = e.ToString();
+        }
+
+        private static void UpdateLogLevel(RadioGroup.SelectedItemChangedArgs SelectedItemChangedArgs)
+        {
+            // "_Error", "_Warn", "_Info", "_Debug", "_Trace"
+            switch (SelectedItemChangedArgs.SelectedItem)
+            {
+                case 0:
+                    MainConfig.Logging["level"] = "error";
+                    break;
+
+                case 1:
+                    MainConfig.Logging["level"] = "warn";
+                    break;
+
+                case 2:
+                    MainConfig.Logging["level"] = "info";
+                    break;
+
+                case 3:
+                    MainConfig.Logging["level"] = "debug";
+                    break;
+
+                case 4:
+                    MainConfig.Logging["level"] = "trace";
+                    break;
+
+                default: //If missing or invalid - Set to info.
+
+                    MainConfig.Logging["level"] = "info";
+                    break;
+            }
+        }
+
+        private static void UpdateEmailFrom(ustring e)
+        {
+            MainConfig.Email["defaultFrom"] = e.ToString();
+        }
+
+        private static void UpdateEmailHost(ustring e)
+        {
+            MainConfig.EmailTransport["host"] = e.ToString();
+        }
+
+        private static void UpdateEmailPort(TextField EmailPort, ustring e)
+        {
+            // Don't allow more than 4 digits and Match Digits only
+            if (SharedFunctions.PortCheck(EmailPort.Text))
+            {
+                var cp = EmailPort.CursorPosition;
+                EmailPort.Text = e;
+                EmailPort.CursorPosition = Math.Min(cp, EmailPort.Text.RuneCount);
+            }
+
+            int DefaultPort = 8810; //default port
+
+            if (Int32.TryParse(EmailPort.Text.ToString(), out int ParsedPort))
+            {
+                if (ParsedPort > 65535) EmailPort.Text = "65535";
+                MainConfig.EmailTransport["port"] = ParsedPort;
+            }
+            else
+            {
+                MainConfig.EmailTransport["port"] = DefaultPort;
+            }
+        }
+
+        private static void UpdateEmailAuthPass(ustring e)
+        {
+            MainConfig.EmailAuth["pass"] = e.ToString();
+        }
+
+        private static void UpdateEmailAuthUser(ustring e)
+        {
+            MainConfig.EmailAuth["user"] = e.ToString();
+        }
+    }
+}
--- ENiGMAConfig/Program.cs
+++ ENiGMAConfig/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Hjson;
 using Newtonsoft.Json;
-using NStack;
 using System;
 using System.IO;
 
@@ -17,317 +16,6 @@ namespace ENiGMAConfig
         private static void Main(string[] args)
         {
             StartGUI();
-        }
-
-        private static void UpdateLogPath(ustring e)
-        {
-            MainConfig.Paths["logs"] = e.ToString();
-        }
-
-        private static void UpdateLogFileame(ustring e)
-        {
-            MainConfig.Logging["fileName"] = e.ToString();
-        }
-
-        private static void UpdateWSPort(TextField WSPort, ustring e)
-        {
-            // Don't allow more than 4 digits and Match Digits only
-            if (SharedFunctions.PortCheck(WSPort.Text))
-            {
-                var cp = WSPort.CursorPosition;
-                WSPort.Text = e;
-                WSPort.CursorPosition = Math.Min(cp, WSPort.Text.RuneCount);
-            }
-
-            int DefaultPort = 8810; //default port
-
-            if (Int32.TryParse(WSPort.Text.ToString(), out int ParsedPort))
-            {
-                if (ParsedPort > 65535) WSPort.Text = "65535";
-                MainConfig.LoginServersWS["port"] = ParsedPort;
-            }
-            else
-            {
-                MainConfig.LoginServersWS["port"] = DefaultPort;
-            }
-        }
-
-        private static void UpdateWSSPort(TextField WSSPort, ustring e)
-        {
-            // Don't allow more than 4 digits and Match Digits only
-            if (SharedFunctions.PortCheck(WSSPort.Text))
-            {
-                var cp = WSSPort.CursorPosition;
-                WSSPort.Text = e;
-                WSSPort.CursorPosition = Math.Min(cp, WSSPort.Text.RuneCount);
-            }
-
-            int DefaultPort = 8811; //default port
-
-            if (Int32.TryParse(WSSPort.Text.ToString(), out int ParsedPort))
-            {
-                if (ParsedPort > 65535) WSSPort.Text = "65535";
-                MainConfig.LoginServersWSS["port"] = ParsedPort;
-            }
-            else
-            {
-                MainConfig.LoginServersWSS["port"] = DefaultPort;
-            }
-        }
-
-        private static void UpdateEmailFrom(ustring e)
-        {
-            MainConfig.Email["defaultFrom"] = e.ToString();
-        }
-
-        private static void UpdateEmailHost(ustring e)
-        {
-            MainConfig.EmailTransport["host"] = e.ToString();
-        }
-
-        private static void UpdateEmailPort(TextField EmailPort, ustring e)
-        {
-            // Don't allow more than 4 digits and Match Digits only
-            if (SharedFunctions.PortCheck(EmailPort.Text))
-            {
-                var cp = EmailPort.CursorPosition;
-                EmailPort.Text = e;
-                EmailPort.CursorPosition = Math.Min(cp, EmailPort.Text.RuneCount);
-            }
-
-            int DefaultPort = 8810; //default port
-
-            if (Int32.TryParse(EmailPort.Text.ToString(), out int ParsedPort))
-            {
-                if (ParsedPort > 65535) EmailPort.Text = "65535";
-                MainConfig.EmailTransport["port"] = ParsedPort;
-            }
-            else
-            {
-                MainConfig.EmailTransport["port"] = DefaultPort;
-            }
-        }
-
-        private static void UpdateEmailAuthPass(ustring e)
-        {
-            MainConfig.EmailAuth["pass"] = e.ToString();
-        }
-
-        private static void UpdateEmailAuthUser(ustring e)
-        {
-            MainConfig.EmailAuth["user"] = e.ToString();
-        }
-
-        private static void UpdatePrivateKeyPath(ustring e)
-        {
-            MainConfig.LoginServersSSH["privateKeyPem"] = e.ToString();
-        }
-
-        private static void UpdatePrivateKeyPass(ustring e)
-        {
-            MainConfig.LoginServersSSH["privateKeyPass"] = e.ToString();
-        }
-
-        private static void UpdatePreloginTheme(ustring e)
-        {
-            MainConfig.Theme["preLogin"] = e.ToString();
-        }
-
-        private static void UpdateDefaultTheme(NStack.ustring e)
-        {
-            MainConfig.Theme["default"] = e.ToString();
-        }
-
-        private static void UpdatePromptFile(NStack.ustring e)
-        {
-            MainConfig.General["promptFile"] = e.ToString();
-        }
-
-        private static void UpdateMenuFile(NStack.ustring e)
-        {
-            string f = e.ToString();
-            MainConfig.General["menuFile"] = f;
-        }
-
-        private static void ButtonEKeyShow_Clicked(Button buttonEKeyShow, TextField textFieldEmailAuthPass)
-        {
-            if (textFieldEmailAuthPass.Secret)
-            {
-                textFieldEmailAuthPass.Secret = false;
-                buttonEKeyShow.Text = "Hide";
-            }
-            else
-            {
-                textFieldEmailAuthPass.Secret = true;
-                buttonEKeyShow.Text = "Show";
-            }
-        }
-
-        private static void ButtonPKeyShow_Clicked(Button ButtonPKeyShow, TextField TextFieldPrivateKeyPass)
-        {
-            if (TextFieldPrivateKeyPass.Secret)
-            {
-                TextFieldPrivateKeyPass.Secret = false;
-                ButtonPKeyShow.Text = "Hide";
-            }
-            else
-            {
-                TextFieldPrivateKeyPass.Secret = true;
-                ButtonPKeyShow.Text = "Show";
-            }
-        }
-
-        private static void UpdateLogLevel(int Selected)
-        {
-            // "_Error", "_Warn", "_Info", "_Debug", "_Trace"
-            switch (Selected)
-            {
-                case 0:
-                    MainConfig.Logging["level"] = "error";
-                    break;
-
-                case 1:
-                    MainConfig.Logging["level"] = "warn";
-                    break;
-
-                case 2:
-                    MainConfig.Logging["level"] = "info";
-                    break;
-
-                case 3:
-                    MainConfig.Logging["level"] = "debug";
-                    break;
-
-                case 4:
-                    MainConfig.Logging["level"] = "trace";
-                    break;
-
-                default: //If missing or invalid - Set to info.
-
-                    MainConfig.Logging["level"] = "info";
-                    break;
-            }
-        }
-
-        private static void UpdateLogLevel(RadioGroup.SelectedItemChangedArgs selectedItemChangedArgs)
-        {
-            // "_Error", "_Warn", "_Info", "_Debug", "_Trace"
-            switch (selectedItemChangedArgs.SelectedItem)
-            {
-                case 0:
-                    MainConfig.Logging["level"] = "error";
-                    break;
-
-                case 1:
-                    MainConfig.Logging["level"] = "warn";
-                    break;
-
-                case 2:
-                    MainConfig.Logging["level"] = "info";
-                    break;
-
-                case 3:
-                    MainConfig.Logging["level"] = "debug";
-                    break;
-
-                case 4:
-                    MainConfig.Logging["level"] = "trace";
-                    break;
-
-                default: //If missing or invalid - Set to info.
-
-                    MainConfig.Logging["level"] = "info";
-                    break;
-            }
-        }
-
-        private static void UpdateLoggingLevel(int Level)
-        {
-        }
-
-        private static void UpdateSSHEnabled(bool SSHChecked)
-        {
-            MainConfig.LoginServersSSH["enabled"] = SSHChecked;
-        }
-
-        private static void UpdateTelnetEnabled(bool TelnetChecked)
-        {
-            MainConfig.LoginServersTelnet["enabled"] = TelnetChecked;
-        }
-
-        private static void UpdateWSEnabled(bool WSChecked)
-        {
-            MainConfig.LoginServersWS["enabled"] = WSChecked;
-        }
-
-        private static void UpdateWSSEnabled(bool WSSChecked)
-        {
-            MainConfig.LoginServersWSS["enabled"] = WSSChecked;
-        }
-
-        private static void UpdateSSHPort(TextField SSHPort, ustring e)
-        {
-            if (SharedFunctions.PortCheck(SSHPort.Text))
-            {
-                var cp = SSHPort.CursorPosition;
-                SSHPort.Text = e;
-                SSHPort.CursorPosition = Math.Min(cp, SSHPort.Text.RuneCount);
-            }
-
-            int DefaultPort = 8889; //default port
-
-            if (Int32.TryParse(SSHPort.Text.ToString(), out int ParsedPort))
-            {
-                if (ParsedPort > 65535) SSHPort.Text = "65535";
-                MainConfig.LoginServersSSH["port"] = ParsedPort;
-            }
-            else
-            {
-                MainConfig.LoginServersSSH["port"] = DefaultPort;
-            }
-        }
-
-        private static void UpdateTelnetPort(TextField TelnetPort, ustring e)
-        {
-            // Don't allow more than 4 digits and Match Digits only
-            if (SharedFunctions.PortCheck(TelnetPort.Text))
-            {
-                var cp = TelnetPort.CursorPosition;
-                TelnetPort.Text = e;
-                TelnetPort.CursorPosition = Math.Min(cp, TelnetPort.Text.RuneCount);
-            }
-
-            int DefaultPort = 8888; //default port
-
-            if (Int32.TryParse(TelnetPort.Text.ToString(), out int ParsedPort))
-            {
-                if (ParsedPort > 65535) TelnetPort.Text = "65535";
-                MainConfig.LoginServersTelnet["port"] = ParsedPort;
-            }
-            else
-            {
-                MainConfig.LoginServersTelnet["port"] = DefaultPort;
-            }
-        }
-
-        private static void UpdateBBSName(object sender, EventArgs e)
-        {
-            TextField TF = (TextField)sender;
-
-            //Name validation if needed
-            string f = TF.Text.ToString();
-
-            MainConfig.General["boardName"] = f;
-            //  TF.Text = "UpdateCheck";
-        }
-
-        private static void UpdateBBSName(NStack.ustring e)
-        {
-            //Name validation if needed
-            string f = e.ToString();
-
-            MainConfig.General["boardName"] = f;
-            //  TF.Text = "UpdateCheck";
         }
 
         private static void ClearViews()
@@ -351,6 +39,26 @@ namespace ENiGMAConfig
             MainConfig.Deserialized = JsonConvert.DeserializeObject<EnigmaConfigSchema>(JSONString);
 
             ProcessConfig(); //File Loaded - Process it.
+        }
+
+        private static void SaveJson(string FilePath)
+        {
+            HjsonOptions FileOptions = new HjsonOptions();
+            FileOptions.KeepWsc = true;
+
+            FileInfo SavePath = new FileInfo(FilePath);
+            if (SavePath.Exists) //Exists - Lets Backup
+            {
+                DirectoryInfo BackupDir = new DirectoryInfo(Path.Combine(SavePath.Directory.FullName, "Backups"));
+                if (!BackupDir.Exists) BackupDir.Create();
+                DateTime CurrentTime = DateTime.Now;
+                string BackupFilename = SavePath.Name + "." + CurrentTime.Year.ToString() + "." + CurrentTime.Month.ToString() + "." + CurrentTime.Day.ToString() + "." + CurrentTime.Minute.ToString() + "." + CurrentTime.Second.ToString() + ".bak";
+
+                string BackupPath = Path.Combine(BackupDir.FullName, BackupFilename);
+                SavePath.CopyTo(BackupPath);
+            }
+
+            HjsonValue.Save(MainConfig.Mainfile, FilePath, FileOptions);
         }
 
         private static void ProcessConfig()
@@ -388,26 +96,6 @@ namespace ENiGMAConfig
 
                 win.Add(LabelNew, Footer);
             }
-        }
-
-        private static void SaveJson(string FilePath)
-        {
-            HjsonOptions FileOptions = new HjsonOptions();
-            FileOptions.KeepWsc = true;
-
-            FileInfo SavePath = new FileInfo(FilePath);
-            if (SavePath.Exists) //Exists - Lets Backup
-            {
-                DirectoryInfo BackupDir = new DirectoryInfo(Path.Combine(SavePath.Directory.FullName, "Backups"));
-                if (!BackupDir.Exists) BackupDir.Create();
-                DateTime CurrentTime = DateTime.Now;
-                string BackupFilename = SavePath.Name + "." + CurrentTime.Year.ToString() + "." + CurrentTime.Month.ToString() + "." + CurrentTime.Day.ToString() + "." + CurrentTime.Minute.ToString() + "." + CurrentTime.Second.ToString() + ".bak";
-
-                string BackupPath = Path.Combine(BackupDir.FullName, BackupFilename);
-                SavePath.CopyTo(BackupPath);
-            }
-
-            HjsonValue.Save(MainConfig.Mainfile, FilePath, FileOptions);
         }
     }
 }
